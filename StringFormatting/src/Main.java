f4f12ee3e360c381a2045aaafc02f29e1972add6
leehaney254 <georgeleehaney20@gmail.com>
2023-12-06 14:08:28 +0300

    Feat: Java strings

---
+++ StringFormatting/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        String bulletIt = "Print a Bulleted List: \n" +
+                "\t\u2022 First Point \n" +
+                "\t\t\u2022 Sub Point";
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                    \u2022 First Point
+                        \u2022 Sub Point""";
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d \n", age);
+
+    }
+}
