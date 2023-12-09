f001978f7f476fefaaec6842a09d18301effdfc1
leehaney254 <georgeleehaney20@gmail.com>
2023-12-09 14:06:15 +0300

    Feat: Java keywords

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        calculateScore("today");
+    }
+
+    public static int calculateScore(String name){
+        System.out.println("Method 2 ran " + name);
+        return 1;
+    }
+}
